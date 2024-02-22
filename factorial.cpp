a92a26d1686c82aa438a9c0448b0523ee001aff6
vinod v <valajaramachandranmuniraj@gmail.com>
2024-02-22 21:15:52 +0530

    done

---
+++ factorial.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    long factorial = 1.0;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    
+        for(int i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        cout << "Factorial of " << n << " is " << factorial;    
+    
+
+    return 0;
+}
