1aece749103f8c04aa448c916b3d5d9ba4d85818
vinod v <valajaramachandranmuniraj@gmail.com>
2024-02-25 16:36:31 +0530

    one more program done

---
+++ even_or_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"enter the value of n:";
+    cin>>n;
+    if(n%2==0){
+        cout<<"n is even number";
+    }
+    else{
+        cout<<"n is odd number";
+    }
+    return 0;
+}
