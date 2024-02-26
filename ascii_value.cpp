bea745ae94db2a7b4a9c1293e122c1f429c3b4d0
vinod v <valajaramachandranmuniraj@gmail.com>
2024-02-26 13:38:50 +0530

    program executed successfully

---
+++ ascii_value.cpp
@@ -0,0 +1,16 @@
+   
+// C++ Program to find ASCII value of a character 
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    char ch; 
+    cout <<"enter value";
+    cin >>ch; 
+  
+    cout << "The ASCII value of " << ch << " is " << int(ch) 
+         << endl; 
+  
+    return 0; 
+}
