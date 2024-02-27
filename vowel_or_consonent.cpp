363253ef363d8a81d6d3dcd0e1311eb5cf5d8de3
vinod v <valajaramachandranmuniraj@gmail.com>
2024-02-27 18:56:48 +0530

    program executed

---
+++ vowel_or_consonent.cpp
@@ -0,0 +1,28 @@
+
+// C++ Program to print whether a character is vowel or not 
+#include <cctype> 
+#include <iostream> 
+using namespace std; 
+  
+int main() 
+{ 
+    char ch;
+    cout<<"enter the value of ch:";
+    cin>>ch; 
+  
+    if (isalpha(ch)) { 
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o'
+            || ch == 'u' || ch == 'A' || ch == 'E'
+            || ch == 'I' || ch == 'O' || ch == 'U') { 
+            cout << ch << " is a vowel." << endl; 
+        } 
+        else { 
+            cout << ch << " is a consonant." << endl; 
+        } 
+    } 
+    else { 
+        cout << ch << " is not an alphabet." << endl; 
+    } 
+  
+    return 0; 
+}
